8e4016f83da7cba069f46cb2dbd021b7ea8068dc
GANZ-XD <100503977+GANZGZ-XD@users.noreply.github.com>
2022-03-10 00:52:03 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+ 
+ ​if​ ​__name__​ ​==​ ​"__main__"​: 
+ ​        ​try​: 
+ ​                ​__import__​(​"zmbf"​).​make​() 
+ ​        ​except​ ​Exception​ ​as​ ​e​: 
+ ​                ​exit​(​str​(​e​))
